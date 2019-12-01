baa69afbcf6f86979d8364ed1ef04b2969cad738
Rafael <madalozrafael@gmail.com>
2019-11-30 23:06:47 -0300

    all features included

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <Calculator />
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+
+import Calculator from './main/Calculator'
 
 export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
+    components: { Calculator }
 }
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+
+@import url('https://fonts.googleapis.com/css?family=Roboto+Mono:300,300i,400,500&display=swap');
+
+* {
+    font-family: "Roboto Mono", monospace;
+    font-weight: 300;
 }
+
+body {
+    margin: 0;
+    
+}
+
+#app {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    justify-content: center;
+    align-items: center;
+    text-align: center;
+    color: #fff;
+    background: linear-gradient(to right, #304352, #d7d2cc);
+}
+
 </style>
---
+++ src/components/Display.vue
@@ -0,0 +1,34 @@
+<template>
+    <div class="display">
+        {{ value | exponential }}
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['value'],
+    filters: {
+        exponential(number) {
+            if(number.length > 8) {
+                return parseFloat(number).toExponential(4)
+            } else {
+                return number
+            }
+        }
+    },
+}
+</script>
+
+<style>
+    .display {
+        grid-column: span 4;
+        background-color: #0004;
+
+        display: flex;
+        justify-content: flex-end;
+        align-items: center;
+        padding: 20px;
+        font-size: 2em;
+        overflow: hidden;
+    }
+</style>
